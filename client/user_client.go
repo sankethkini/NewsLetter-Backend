e8a9df23be2397b7da68a5d71787cd1d7ba75a8b
sankethkini <kinisanketh@gmail.com>
2022-02-14 16:04:31 +0530

    client and service registry

--- client/user_client.go
+++ client/user_client.go
@@ -4,11 +4,13 @@ import (
 	"context"
 	"fmt"
 
+	subscriptionpb "github.com/sankethkini/NewsLetter-Backend/proto/subscriptionpb/v1"
 	userpb "github.com/sankethkini/NewsLetter-Backend/proto/userpb/v1"
 	"google.golang.org/grpc"
 )
 
-func main() {
+// nolint: staticcheck
+func usr() {
 	serverAddress := "localhost:9000"
 
 	conn, e := grpc.Dial(serverAddress, grpc.WithInsecure())
@@ -34,4 +36,25 @@ func main() {
 	fmt.Println(resp, err)
 	resp1, err := cl.ValidateUser(ctx, &data1)
 	fmt.Println(resp1, err)
+}
+
+// nolint: staticcheck
+func main() {
+	serverAddress := "localhost:9000"
+
+	conn, e := grpc.Dial(serverAddress, grpc.WithInsecure())
+
+	if e != nil {
+		panic(e)
+	}
+	defer conn.Close()
+	cl := subscriptionpb.NewSubscriptionServiceClient(conn)
+	data := subscriptionpb.CreateSchemeRequest{
+		Name:  "some",
+		Price: 500,
+		Days:  28,
+	}
+	ctx := context.Background()
+	resp, err := cl.CreateScheme(ctx, &data)
+	fmt.Println(resp, err)
 }
